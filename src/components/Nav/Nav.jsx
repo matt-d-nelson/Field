cf06445285697b8c9ab79bddd8b1b6878c59bf4e
Matt Nelson <mattnelson6992@gmail.com>
2022-07-17 16:20:33 -0500

    Updated Nav componenet view 2.0/ all components needed for base mode crud created / feature complete

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -14,25 +14,32 @@ function Nav() {
         <h2 className="nav-title">Prime Solo Project</h2>
       </Link>
       <div>
-        {/* If no user is logged in, show these links */}
+        {/* ------------------UNPROTECTED LINKS------------------ */}
         {!user.id && (
-          // If there's no user, show login/registration links
-          <Link className="navLink" to="/login">
-            Login / Register
-          </Link>
+          <>
+            {/* // If there's no user, show login/registration links */}
+            <Link className="navLink" to="/login">
+              Login
+            </Link>
+            <Link className="navLink" to="/explore">
+              Explore
+            </Link>
+          </>
         )}
 
+        {/* ------------------PROTECTED LINKS------------------ */}
         {/* If a user is logged in, show these links */}
         {user.id && (
           <>
             <Link className="navLink" to="/user">
-              Home
+              Profile
             </Link>
-
-            <Link className="navLink" to="/info">
-              Info Page
+            <Link className="navLink" to="/new">
+              New Post
             </Link>
-
+            <Link className="navLink" to="/explore">
+              Explore
+            </Link>
             <LogOutButton className="navLink" />
           </>
         )}
