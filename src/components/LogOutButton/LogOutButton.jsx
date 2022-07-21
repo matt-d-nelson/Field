a94d05bd56f800baffb32e7715f2e7d24b61f5f5
Matt Nelson <mattnelson6992@gmail.com>
2022-07-21 10:30:42 -0500

    cleared post reducer on logout so that forign user posts don't flash on login

--- src/components/LogOutButton/LogOutButton.jsx
+++ src/components/LogOutButton/LogOutButton.jsx
@@ -4,11 +4,14 @@ import { useDispatch } from "react-redux";
 
 function LogOutButton(props) {
   const dispatch = useDispatch();
+
+  const onLogout = () => {
+    dispatch({ type: "LOGOUT" });
+    dispatch({ type: "SET_POSTS", payload: [] });
+  };
+
   return (
-    <Button
-      className={props.className}
-      onClick={() => dispatch({ type: "LOGOUT" })}
-    >
+    <Button className={props.className} onClick={onLogout}>
       Log Out
     </Button>
   );
