6bdddd843d06658ef226936feae95cdf52f34b68
Matt Nelson <mattnelson6992@gmail.com>
2022-07-21 08:28:33 -0500

    DELETE - successfull delete query / DELETE complete

--- server/routes/asset.router.js
+++ server/routes/asset.router.js
@@ -94,9 +94,21 @@ router.put(
 );
 
 // delete
-router.delete("/:id", (req, res) => {
+router.delete("/:id", rejectUnauthenticated, (req, res) => {
   console.log("DELETE", req.params.id);
-  res.send("ribbit");
+
+  const deletePostId = [req.params.id];
+  const deletePostQuery = `DELETE FROM "post" WHERE id = $1;`;
+
+  pool
+    .query(deletePostQuery, deletePostId)
+    .then((result) => {
+      res.sendStatus(200);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.sendStatus(500);
+    });
 });
 
 //---------------------HELPER FUNCTIONS---------------------//
