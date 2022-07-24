422b8ec8ca44596321eb360c5334c51b0f443c01
Matt Nelson <mattnelson6992@gmail.com>
2022-07-24 14:22:18 -0500

    created a reducer to store tags for a specific post

--- src/redux/reducers/_root.reducer.js
+++ src/redux/reducers/_root.reducer.js
@@ -4,6 +4,7 @@ import user from "./user.reducer";
 import modal from "./modal.reducer";
 import posts from "./posts.reducer";
 import followedPosts from "./followedPosts.reducer";
+import tags from "./tags.reducer";
 
 // rootReducer is the primary reducer for our entire project
 // It bundles up all of the other reducers so our project can use them.
@@ -17,6 +18,7 @@ const rootReducer = combineReducers({
   modal, // creates a globally rendered dialog popup
   posts, // contains all posts or all posts of a specific user
   followedPosts, // contains all posts of users that the currently logged in user is following
+  tags, // contains an array of tags for a specific post
 });
 
 export default rootReducer;
