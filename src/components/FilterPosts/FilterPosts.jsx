8caee3b5317f88fd79257cea36e141a439c386d3
Matt Nelson <mattnelson6992@gmail.com>
2022-07-24 13:43:47 -0500

    created FilterPosts component / used in MapView

---
+++ src/components/FilterPosts/FilterPosts.jsx
@@ -0,0 +1,11 @@
+import { Typography } from "@material-ui/core";
+
+function FilterPosts() {
+  return (
+    <div>
+      <Typography>filter</Typography>
+    </div>
+  );
+}
+
+export default FilterPosts;
