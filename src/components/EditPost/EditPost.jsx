76b4f25f91a1e54a0cbc9ccde2bcb67823689fc0
Matt Nelson <mattnelson6992@gmail.com>
2022-07-27 16:57:15 -0500

    completed styling for EditPost view 3.1

--- src/components/EditPost/EditPost.jsx
+++ src/components/EditPost/EditPost.jsx
@@ -31,9 +31,9 @@ function EditPost() {
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
   const [picture, setPicture] = useState("");
-  const [picturePath, setPicturePath] = useState("upload image");
+  const [picturePath, setPicturePath] = useState("(none selected)");
   const [audio, setAudio] = useState("");
-  const [audioPath, setAudioPath] = useState("upload audio");
+  const [audioPath, setAudioPath] = useState("(none selected)");
   const [tags, setTags] = useState("");
   // url params
   const thisPostId = useParams();
@@ -154,7 +154,7 @@ function EditPost() {
     if (
       fileExtension === "mp3" ||
       fileExtension === "wav" ||
-      fileExtension === "upload audio"
+      fileExtension === "(none selected)"
     ) {
       return false;
     }
@@ -169,7 +169,7 @@ function EditPost() {
       fileExtension === "jpeg" ||
       fileExtension === "png" ||
       fileExtension === "gif" ||
-      fileExtension === "upload image"
+      fileExtension === "(none selected)"
     ) {
       return false;
     }
@@ -190,9 +190,14 @@ function EditPost() {
   return (
     <div>
       {thisPost != undefined ? (
-        <div style={{ padding: "20px", marginLeft: "5%", marginRight: "5%" }}>
+        <div
+          style={{
+            paddingTop: "90px",
+            marginLeft: "30px",
+            marginRight: "30px",
+          }}
+        >
           <Typography variant="h3">Edit Post</Typography>
-          <p>View 3.1</p>
           <Grid container spacing={2}>
             <Grid item xs={6}>
               <MapCreate
@@ -205,6 +210,7 @@ function EditPost() {
               <Grid container direction="column" spacing={1}>
                 <Grid item>
                   <TextField
+                    inputProps={{ style: { fontSize: 30 } }}
                     variant="filled"
                     label="title"
                     fullWidth
@@ -216,8 +222,9 @@ function EditPost() {
                   <TextField
                     variant="filled"
                     label="description"
+                    inputProps={{ style: { fontSize: 30 } }}
                     multiline
-                    minRows={10}
+                    minRows={8}
                     fullWidth
                     value={description}
                     onChange={onDescriptionChange}
@@ -225,6 +232,7 @@ function EditPost() {
                 </Grid>
                 <Grid item>
                   <TextField
+                    inputProps={{ style: { fontSize: 30 } }}
                     variant="filled"
                     label="tags"
                     fullWidth
@@ -232,39 +240,92 @@ function EditPost() {
                     onChange={onTagsChange}
                   />
                 </Grid>
+                <Grid container justifyContent="space-evenly">
+                  <Grid item sx={6}>
+                    <Typography variant="h5">file: {audioPath}</Typography>
+                  </Grid>
+                  <Grid item sx={6}>
+                    <Typography variant="h5">file: {picturePath}</Typography>
+                  </Grid>
+                </Grid>
                 <Grid item>
-                  <ButtonGroup fullWidth>
-                    <Button component="label" variant="contained">
+                  <ButtonGroup fullWidth style={{ marginBottom: "0px" }}>
+                    <Button
+                      component="label"
+                      variant="outlined"
+                      style={{
+                        border: "4px solid",
+                        padding: "0px",
+                      }}
+                    >
                       <input
                         name="audio"
                         type="file"
                         onChange={onAudioChange}
                         hidden
                       />
-                      <Typography>{audioPath}</Typography>
+                      <Typography variant="h4">upload audio</Typography>
                     </Button>
-                    <Button component="label" variant="contained">
+                    <Button
+                      component="label"
+                      variant="outlined"
+                      style={{
+                        border: "4px solid",
+                        padding: "0px",
+                        marginLeft: "20px",
+                      }}
+                    >
                       <input
                         type="file"
                         name="picture"
                         onChange={onPictureChange}
                         hidden
                       />
-                      <Typography>{picturePath}</Typography>
+                      <Typography variant="h4">upload image</Typography>
                     </Button>
                   </ButtonGroup>
                 </Grid>
               </Grid>
             </Grid>
             <Grid item xs={12}>
-              <Button variant="outlined" onClick={onCancel}>
-                <Typography>cancel</Typography>
+              <Button
+                variant="outlined"
+                onClick={onCancel}
+                style={{
+                  border: "4px solid",
+                  padding: "0px",
+                  marginLeft: "7px",
+                  width: "200px",
+                }}
+                color="info"
+              >
+                <Typography variant="h4">cancel</Typography>
               </Button>
-              <Button variant="outlined" onClick={onDelete}>
-                <Typography>delete</Typography>
+              <Button
+                variant="outlined"
+                onClick={onDelete}
+                style={{
+                  border: "4px solid",
+                  padding: "0px",
+                  marginLeft: "7px",
+                  width: "200px",
+                }}
+                color="secondary"
+              >
+                <Typography variant="h4">delete</Typography>
               </Button>
-              <Button variant="outlined" onClick={onUpdate}>
-                <Typography>update</Typography>
+              <Button
+                variant="outlined"
+                onClick={onUpdate}
+                style={{
+                  border: "4px solid",
+                  padding: "0px",
+                  marginLeft: "7px",
+                  width: "200px",
+                }}
+                color="primary"
+              >
+                <Typography variant="h4">update</Typography>
               </Button>
             </Grid>
           </Grid>
